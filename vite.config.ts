103110db7164768803bc3f9cdbf3ef3e2f759870
Andrew Wesbecher <andrew@madrone.capital>
2025-04-20 22:34:04 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig({
-  plugins: [react(), componentTagger()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    componentTagger() // Required by Lovable's visual editor
+  ],
+  build: {
+    target: "esnext",
+    outDir: "dist"
+  }
 });
