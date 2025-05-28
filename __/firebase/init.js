b9a2cffcd7aa61cc86fc3a002208657e2b6e2e52
awesbecher <andrew@madrone.capital>
2025-05-28 12:36:48 -0700

    Restore prod build from Firebase v292252

---
+++ __/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyDrxoFrkBILefok-2VgjenV6ZJhdhGUO2A",
+  "authDomain": "powered-by-website-redundancy.firebaseapp.com",
+  "databaseURL": "",
+  "messagingSenderId": "326818403042",
+  "projectId": "powered-by-website-redundancy",
+  "storageBucket": "powered-by-website-redundancy.firebasestorage.app"
+});
