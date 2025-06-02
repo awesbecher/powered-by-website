88cb4b7231296b8d1b06a70c4fa3ba00cbd7b9ed
awesbecher <andrew@madrone.capital>
2025-06-02 12:19:06 -0700

    Deploy: Update 'What's an AI Agent?' video to new ID h-rpV6f0JJs

--- __/firebase/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-  "apiKey": "AIzaSyDrxoFrkBILefok-2VgjenV6ZJhdhGUO2A",
-  "authDomain": "powered-by-website-redundancy.firebaseapp.com",
-  "databaseURL": "",
-  "messagingSenderId": "326818403042",
-  "projectId": "powered-by-website-redundancy",
-  "storageBucket": "powered-by-website-redundancy.firebasestorage.app"
-});
